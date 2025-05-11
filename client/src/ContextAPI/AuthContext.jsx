fe1b8c2d52ee5b656779a8cc4d6a5369381db4b1
Kishan Vishnu Harode <122368262+KishanHarode@users.noreply.github.com>
2025-05-11 09:29:21 +0530

    Update AuthContext.jsx

--- client/src/ContextAPI/AuthContext.jsx
+++ client/src/ContextAPI/AuthContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext } from 'react'
 export const AuthContext_API = createContext();
 
 const AuthContext = ({children}) => {
-    const serverURL = "http://localhost:3000";
+    const serverURL = "https://mern-stack-employee-management-system-qv7v.onrender.com/";
     const value = {
         serverURL,
     }
